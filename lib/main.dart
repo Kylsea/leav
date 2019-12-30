35c920376dde3312adc805037660085147e6ad61
Nathan David <nathan@david.ca>
2019-12-30 03:55:21 -0500

    v1.0.3 - Design
    - Added Slider
    - Added Text
    - Added - Page Swipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,121 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  double _sliderValue = 15.0;
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Column(
+          child: PageView(
+            scrollDirection: Axis.vertical,
             children: <Widget>[
-              new Container(
-                width: 216.0,
-                height: 79.0,
-                child: Image.asset('images/logo.png'),
-              ),
-              SizedBox(height: 47.0),
-              Text(
-                '179.40',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 60.0,
-                  letterSpacing: 2.5,
-                  color: Color(0xFF01B189),
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 47.0),
-              new Container(
-                width: 216.0,
-                height: 216.0,
-                decoration: new BoxDecoration(
-                    // Circle
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    // Border
-                    border: new Border.all(
-                      width: 2.0,
-                      color: Colors.white,
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset('images/logo.png'),
+                      width: 216.0,
+                      height: 79.0,
                     ),
-                    // Shadow
-                    boxShadow: [
-                      new BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 5.0,
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          child: Image.asset('images/pay'),
+                          width: 216.0,
+                          height: 216.0,
+                          decoration: new BoxDecoration(
+                              // Circle
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                              // Border
+                              border: new Border.all(
+                                width: 2.0,
+                                color: Colors.white,
+                              ),
+                              // Shadow
+                              boxShadow: [
+                                new BoxShadow(
+                                  color: Colors.black.withOpacity(0.2),
+                                  blurRadius: 5.0,
+                                ),
+                              ]),
+                        ),
                       ),
-                    ]),
-                child: Icon(Icons.credit_card, size: 100.0),
-              ),
-              SizedBox(height: 47.0),
-              Center(
-                child: Text(
-                  'Tip',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    letterSpacing: 2.5,
-                    color: Color(0xFF737375),
-                  ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('Tip'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        child: Container(
+                          width: 247.0,
+                          height: 25.0,
+                          child: SliderTheme(
+                            data: SliderThemeData(
+                              trackHeight: 10.0,
+                              activeTrackColor: Color(0xFF00C598),
+                              inactiveTrackColor: Color(0xFFB2EDDF),
+                            ),
+                            child: Slider(
+                              value: _sliderValue,
+                              onChanged: (newValue) {
+                                setState(() {
+                                  
+                                });
+                              },
+                              divisions: 10,
+                              max: 100.00,
+                              min: 0.00,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('View Bill'),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Icon(
+                          Icons.keyboard_arrow_up,
+                          color: Color(0xFF707070),
+                          size: 30.0,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          color: Color(0xFF01B189),
+                          height: 31.0,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+              ),
+              Container(
+                child: Center(
+                  child: Text("Page 2"),
+                ),
+                color: Colors.white,
               ),
             ],
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
+
+void setState(Null Function() param0) {}
